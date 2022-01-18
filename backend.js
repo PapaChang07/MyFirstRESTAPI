a38bef965a98048b243d8f5e0faaefb767de1f29
Norman Chang <sangheilios081@gmail.com>
2022-01-18 10:49:22 -0800

    HOPE

--- backend.js
+++ backend.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { status } = require('express/lib/response');
 const res = require('express/lib/response');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 const port = 5000;
 
@@ -37,7 +37,7 @@ const users = {
     ]
 }
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
