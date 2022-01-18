a7d781dbffcfe14f153963a09f8205ec8637f41a
Norman Chang <sangheilios081@gmail.com>
2022-01-17 17:43:40 -0800

    Testing

--- backend.js
+++ backend.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { status } = require('express/lib/response');
 const res = require('express/lib/response');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
@@ -36,6 +37,7 @@ const users = {
     ]
 }
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
